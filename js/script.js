70aba7f7f442d3a334c44346149e1be4b4246329
Vlad <tixon16@rambler.ru>
2019-04-09 22:55:20 +0300

    day2

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 const switcher = document.querySelector('#cbx'),
       more = document.querySelector('.more'),
       modal = document.querySelector('.modal'),
-      videos = document.querySelectorAll('.videos-item');
+      videos = document.querySelectorAll('.videos__item');
 let  player;
 
 function bindSlideToggle(trigger, boxBody, content, openClass) {
@@ -91,5 +91,78 @@ more.addEventListener('click', () => {
         setTimeout(() => {
             card.classList.remove('videos__item-active');
         }, 10);
+        bindNewModal(card);
+    }
+    sliceTitle('.videos__item-descr', 100);
+});
+
+function sliceTitle(selector, count) {
+    document.querySelectorAll(selector).forEach(item => {
+        item.textContent.trim();
+        if (item.textContent.length < 100) {
+            return;
+        } else {
+            const str = item.textContent.slice(0, count + 1) + "...";
+            item.textContent = str;
+        }
+    });
+}
+sliceTitle('.videos__item-descr', 100);
+
+function openModal() {
+    modal.style.display = 'block';
+}
+
+function closeModal() {
+    modal.style.display = 'none';
+    player.stopVideo();
+}
+
+function bindModal(cards) {
+    cards.forEach(item => {
+        item.addEventListener('click', (event) => {
+            event.preventDefault();
+            const id = item.getAttribute('data-url');
+            loadVideo(id);
+            openModal();
+        });
+    });
+}
+bindModal(videos);
+
+function bindNewModal(cards) {
+    cards.addEventListener('click', (event) => {
+        event.preventDefault();
+        const id = cards.getAttribute('data-url');
+        loadVideo(id);
+        openModal();
+    });
+}
+
+modal.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('modal__body')) {
+        closeModal();
     }
 });
+
+function createVideo() {
+    var tag = document.createElement('script');
+
+    tag.src = "https://www.youtube.com/iframe_api";
+    var firstScriptTag = document.getElementsByTagName('script')[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    setTimeout(() => {
+        player = new YT.Player('frame', {
+            height: '100%',
+            width: '100%',
+            videoId: 'M7lc1UVf-VE'
+        });
+    }, 300);
+    
+}
+createVideo();
+
+function loadVideo(id) {
+    player.loadVideoById({'videoId': `${id}`});
+}
